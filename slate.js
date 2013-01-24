146e456f151cf86df1f5548154abe4f7d322bb42
Bradley Wright <brad@intranation.com>
2013-01-24 07:45:49 +0000

    slate.js: More debugging, TODOs

--- slate.js
+++ slate.js
@@ -12,35 +12,26 @@ slate.config('checkDefaultsOnLoad', true);
 var hostname = slate.shell('/bin/hostname', true),
     // default resolution - suitable for MacBook Air 13" and MacBook Pro 15"
     macResolution = '1440x900';
-var oneScreenLayout = slate.layout('oneScreen', {
-    'Emacs': {
-        'operations': [
-            S.op("move", {
-                'x': '(screenSizeX-821)/2',
-                'y': '0',
-                'width': '821',
-                'height': '871'
-            })
-        ]
-    },
-    // TODO
-    'Mail': {
-        'operations': "move (screenSizeX-1150)/2;screenOriginY+(screenSizeY-740)/2 1150;740" + macResolution
-    },
-    // TODO
-    'iTerm': {
-        'operations': "move (screenSizeX-1130)/2;screenOriginY+(screenSizeY-556)/2 1130;556 " + macResolution
-    }
-});
-if (hostname === 'kernel') {
+
+// FIXME: why doesn't this hostname check work?
+if (hostname === "kernel" || true) {
     // home MacBook Air
-    var oneScreenLayout = slate.layout('oneScreen', {
+    var oneScreenLayout = S.layout('oneScreen', {
         'Emacs': {
-            'operations': "move (screenSizeX-821)/2;0 821;871 " + macResolution
+            'operations': [
+                S.op("move", {
+                    'x': '(screenSizeX-821)/2',
+                    'y': '0',
+                    'width': '821',
+                    'height': '871'
+                })
+            ]
         },
+        // TODO
         'Mail': {
             'operations': "move (screenSizeX-1150)/2;screenOriginY+(screenSizeY-740)/2 1150;740" + macResolution
         },
+        // TODO
         'iTerm': {
             'operations': "move (screenSizeX-1130)/2;screenOriginY+(screenSizeY-556)/2 1130;556 " + macResolution
         }
