60d6e9bf680cdbf4143607360d486a39591d6aab
Bradley Wright <brad@intranation.com>
2013-01-28 15:33:40 +0000

    Audo adapt to external screen on GDS laptop

--- slate.js
+++ slate.js
@@ -2,7 +2,8 @@
 // https://github.com/jigish/slate/wiki/JavaScript-Configs
 
 /*jslint
-  nomen: true
+  nomen: true,
+  white: true
 */
 /*globals slate, _, S */
 
@@ -11,13 +12,16 @@ slate.config('checkDefaultsOnLoad', true);
 
 var hostname = slate.shell('/bin/hostname', true).trim(),
     // default resolution - suitable for MacBook Air 13" and MacBook Pro 15"
-    macResolution = '1440x900';
+    macResolution = '1440x900',
+    viewSonicResolution = '1920x1080',
+    oneScreenLayout,
+    oneExternalScreenLayout;
+
 
 // FIXME: why doesn't this hostname check work?
 if (hostname === "kernel") {
     // home MacBook Air
-    var oneScreenLayout = S.layout('oneScreen', {
-        // show Emacs in the centre, 100 columns wide in current font
+    oneScreenLayout = S.layout('oneScreen', {
         'Emacs': {
             'operations': [
                 S.op("move", {
@@ -52,7 +56,46 @@ if (hostname === "kernel") {
         }
     });
 }
+else if (hostname.indexOf('GDS') !== -1) {
+    // GDS
+    oneScreenLayout = S.layout('oneScreen', {
+        'iTerm': {
+            'operations': [
+                S.op("move", {
+                    'x': 'screenOriginX',
+                    'y': 'screenOriginY',
+                    'width': 'screenSizeX',
+                    'height': 'screenSizeY'
+                })
+            ]
+        }
+    });
+    oneExternalScreenLayout = S.layout('externalViewSonicScreen', {
+        'Google Chrome': {
+            'operations': [
+                S.op('move', {
+                    'x': 'screenOriginX',
+                    'y': 'screenOriginY',
+                    'width': '1344',
+                    'height': '1054'
+                })
+            ]
+        },
+        'Flint': {
+            'operations': [
+                S.op('move', {
+                    'x': 'screenOriginX + 1344',
+                    'y': 'screenOriginY',
+                    'width': '576',
+                    'height': '1054'
+                })
+            ]
+        }
+    });
+    S.def([viewSonicResolution], "externalViewSonicScreen");
+}
 
+S.def([macResolution], "oneScreen");
 slate.bind('h:ctrl;alt;cmd', slate.operation('layout', {'name': oneScreenLayout}));
 slate.bind('r:ctrl;alt;cmd', slate.operation('relaunch'));
 
